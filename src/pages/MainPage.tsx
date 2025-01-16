26f9976e856cdd3f2e397b8a7e2e05d01775cec9
rusi444 <125140920+rusi444@users.noreply.github.com>
2025-01-16 11:20:17 +0000

    second-commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../shared/ui/Input/Input";
 import Button from "../shared/ui/Button/Button";
+import weatherRequest from "../shared/api/api";
+
 
 const MainPage = () => {
     const [city, setCity] = useState(""); 
     const button = () => { setCity() }
     return (
         <div className="App">
-            {/* <Input ></Input> */}
+            {/<Input type="text" value="жматиь" onClick={weatherRequest}></Input> }
             <Button handleClick={button}></Button>
         </div>
     )
---
+++ src/shared/api/api.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { ITemperature, Temperature } from "../temperature/temperature";
+
+
+const axiosWeatherInstance = axios.create({
+    baseURL: "https://api.openweathermap.org/data/2.5/forecast?q=Omsk&lang=ru&units=metric&appid=5fd59655896311dc6b576f585c3dfaf6"
+})
+
+const weatherRequest = {
+        cityReq: async(cityName: string) =>{
+            const response = await axiosWeatherInstance.get<ICityRDO[]>("locations/v1/cities/search", {
+                withCredentials: false,
+                params:{
+                    apikey: "2cbbc3d76457890139f4fd384cf18565" ,
+                    q: cityName
+                }
+            })
+            return response.data
+        },
+        weatherReq: async(cityKey: string) =>{
+            const response = await axiosWeatherInstance.get<ITemperature[]>("forecasts/v1/hourly/1hour/"+cityKey,{
+                withCredentials: false,
+                params:{
+                    apikey: "2cbbc3d76457890139f4fd384cf18565",
+                    metric: true
+                }
+            })
+            return response.data
+        }
+        
+    }
+
+
+export default weatherRequest
